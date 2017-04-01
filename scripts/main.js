fc8d144c0aa7b17223afe9494b334939c829fe79
Gabbyjose <gabbyjose@gmail.com>
2017-04-01 13:44:17 -0400

    Re-formatted map model, added modals
    All maps now render from the beginning and either have a display of
    none, or not. Also added modals for France, Argentina and Spain with
    some basic information

--- scripts/main.js
+++ scripts/main.js
@@ -17,22 +17,6 @@ $(function(){
 
 //This is the drill down functionality
 
-  function testClick(event, code){
-    console.log('testing the click funtion');
-    handleRegionClick(event, code);
-    new jvm.MultiMap({
-      container: $('#modalMap'),
-      maxLevel: 1,
-      main: {
-        map: 'world_mill'
-      },
-      mapUrlByCode: function(code, multiMap){
-        console.log('is the URL working?');
-        return 'scripts/jquery-jvectormap-fr_regions_mill.js';
-      }
-  });  
-  }
-
   var worldMap = {
     map: 'world_mill',
     backgroundColor: 'transparent',
@@ -52,6 +36,97 @@ $(function(){
     }
   }
 
+  var franceMap = {
+    map: 'fr_regions_mill',
+    backgroundColor: 'transparent',
+    zoomOnScroll: false,
+    regionStyle: {
+      initial: {
+        fill: 'white',
+        "fill-opacity": 1,
+        stroke: 'none',
+        "stroke-width": 0,
+        "stroke-opacity": 1
+      },
+      hover: {
+        "fill-opacity": 0.8,
+        cursor: 'pointer'
+      }
+    },
+    series: {
+      regions: [{
+        values: {
+          'FR-F': '#9932CC',
+          'FR-B': 'indigo',
+          'FR-D': '#9400D3',
+          'FR-V': '#9932CC'
+        },
+      attribute: 'fill'
+    }]
+  }
+  }
+
+
+  var spainMap = {
+    map: 'es_mill',
+    backgroundColor: 'transparent',
+    zoomOnScroll: false,
+    regionStyle: {
+      initial: {
+        fill: 'white',
+        "fill-opacity": 1,
+        stroke: 'none',
+        "stroke-width": 0,
+        "stroke-opacity": 1
+      },
+      hover: {
+        "fill-opacity": 0.8,
+        cursor: 'pointer'
+      }
+    }
+  }
+
+
+  var argentinaMap = {
+    map: 'ar_mill',
+    backgroundColor: 'transparent',
+    zoomOnScroll: false,
+    regionStyle: {
+      initial: {
+        fill: 'white',
+        "fill-opacity": 1,
+        stroke: 'none',
+        "stroke-width": 0,
+        "stroke-opacity": 1
+      },
+      hover: {
+        "fill-opacity": 0.8,
+        cursor: 'pointer'
+      }
+    }
+  }
+
+
+  var usMap = {
+    map: 'us_aea',
+    backgroundColor: 'transparent',
+    zoomOnScroll: false,
+    regionStyle: {
+      initial: {
+        fill: 'white',
+        "fill-opacity": 1,
+        stroke: 'none',
+        "stroke-width": 0,
+        "stroke-opacity": 1
+      },
+      hover: {
+        "fill-opacity": 0.8,
+        cursor: 'pointer'
+      }
+    }
+  }
+
+
   worldMap.series = {
     regions: [{
       values: {
@@ -69,10 +144,16 @@ $(function(){
     }]
   }
 
-  worldMap.onRegionClick = testClick;
-  $('#world-map').vectorMap(worldMap);
+  worldMap.onRegionClick = handleRegionClick;
+  franceMap.onRegionClick = handleSubRegionClick;
+  argentinaMap.onRegionClick = handleSubRegionClick;
+  spainMap.onRegionClick = handleSubRegionClick;
 
-  
+  $('#world-map').vectorMap(worldMap);
+  $('#france-map').vectorMap(franceMap);
+  $('#us-map').vectorMap(usMap);  
+  $('#spain-map').vectorMap(spainMap);
+  $('#argentina-map').vectorMap(argentinaMap);
 
   //reset map focus to the origin of 0,0
   $('button').click(reset);
@@ -80,40 +161,52 @@ $(function(){
    function handleRegionClick(event, code) {
      //This will be the code to zoom in on a country when selected if a region map is available
      if (code === 'FR'){
-      countryMap.map = 'fr_regions_mill';
-      countryName = 'France';
-      callCountryMap(countryMap, countryName);
+      callCountryMap('franceMap', 'france', code);
+
+      // focusOnClickedCountry(code);
+      // $('#france-map').toggleClass('hide');
+      // hideWorldMap();
+      // countryMap = 'francMap';
+      // countryName = 'France';
+      // callCountryMap(countryMap, countryName);
      }
 
      else if (code === 'US'){
-      countryMap.map = 'us_aea';
-      countryName = 'United States';
-      callCountryMap(countryMap, countryName);    
+      callCountryMap('usMap', 'us', code);
+      // countryMap.map = 'us_aea';
+      // countryName = 'United States';
+      // callCountryMap(countryMap, countryName);    
      }
 
      else if (code === 'ES'){
-      countryMap.map = 'es_mill';
-      countryName = 'Spain';
-      callCountryMap(countryMap, countryName);
+      callCountryMap('spainMap', 'spain', code);
+      // countryMap.map = 'es_mill';
+      // countryName = 'Spain';
+      // callCountryMap(countryMap, countryName);
      }
 
      else if (code === 'AR'){
-      countryMap.map = 'ar_mill';
-      countryName = 'Argentina';
-      callCountryMap(countryMap, countryName);
+      callCountryMap('argentinaMap', 'argentina', code);
+      // countryMap.map = 'ar_mill';
+      // countryName = 'Argentina';
+      // callCountryMap(countryMap, countryName);
      }
 
-     else if (code === 'IT'){
-      countryMap.map = 'it_regions_mill';
-      countryName = 'Italy';
-      callCountryMap(countryMap, countryName);
-     }
+     // else if (code === 'IT'){
+     //  hideWorldMap():
+     //  $('#france-map').vectorMap(franceMap);
+     //  countryMap.map = 'it_regions_mill';
+     //  countryName = 'Italy';
+     //  callCountryMap(countryMap, countryName);
+     // }
 
-     else if (code === 'ZA'){
-      countryMap.map = 'za_mill';
-      countryName = 'South Africa';
-      callCountryMap(countryMap, countryName);
-     }
+     // else if (code === 'ZA'){
+     //  hideWorldMap():
+     //  $('#france-map').vectorMap(franceMap);
+     //  countryMap.map = 'za_mill';
+     //  countryName = 'South Africa';
+     //  callCountryMap(countryMap, countryName);
+     // }
 
      else {
       focusOnClickedCountry(code);
@@ -121,35 +214,110 @@ $(function(){
    }
 
   function reset (){
-     $('#world-map').vectorMap('set', 'focus', {
-      scale: 1,
-      x: 0,
-      y: 0,
-      animate: true     
-    });
+     if ($('#world-map').hasClass('hide')){
+       $('#world-map').toggleClass('hide');
+       $('#argentina-map').toggleClass('hide', true);
+       $('#france-map').toggleClass('hide', true);
+       $('#spain-map').toggleClass('hide', true);
+       $('#italy-map').toggleClass('hide', true);
+       $('#us-map').toggleClass('hide', true);
+       $('#world-map').vectorMap('set', 'focus', {
+          scale: 1,
+          x: 0,
+          y: 0,
+          animate: true     
+       })       
+    }
+     
+     else {
+        $('#world-map').vectorMap('set', 'focus', {
+          scale: 1,
+          x: 0,
+          y: 0,
+          animate: true     
+        })
+     }
+
+    };
+  
+
+  function hideWorldMap(){
+    console.log('testing this hide feature');
+    $('#world-map').toggleClass('hide');
+
   }
 
- 
-   function callCountryMap(countryMap, countryName){
-      $('.header').html(countryName);
-      $('#modalMap').html('');
-      $('#modalMap').vectorMap(countryMap);
-      // $('#modalMap').updateSize();
-      $('.ui.modal').modal('show');
-   }
+  function callCountryMap(countryMap, countryName, code){
+    // $('.header').html(countryName);
+    focusOnClickedCountry(code);
+    countryMap = '#'+countryName+'-map';
+    $(countryMap).toggleClass('hide');
+    hideWorldMap();
+    $(countryMap).vectorMap('set', 'focus', {
+     /**
+      * The region property takes a string that represents the region. It can
+      * be written in manually, but we are choosing to make it dynamic using a 
+      * variable.
+      */
+     region: code,
+     animate: true
+  });
+    // $('#modalMap').html('');
 
-   function focusOnClickedCountry(code) {
-     $('#world-map').vectorMap('set', 'focus', {
-       /**
-        * The region property takes a string that represents the region. It can
-        * be written in manually, but we are choosing to make it dynamic using a 
-        * variable.
-        */
-       region: code,
-       animate: true
-    });
+    // $('#modalMap').updateSize();
+    // $('.ui.modal').modal('show');
   }
 
+  function focusOnClickedCountry(code) {
+   $('#world-map').vectorMap('set', 'focus', {
+     /**
+      * The region property takes a string that represents the region. It can
+      * be written in manually, but we are choosing to make it dynamic using a 
+      * variable.
+      */
+     region: code,
+     animate: true
+  });
+  }
+
+  function handleSubRegionClick(event, code){
+    console.log(event, code);
+    if (code === "FR-F"){
+      $('.ui.modal.loire').modal('show');   
+    }
+
+    else if (code === "FR-B"){
+      $('.ui.modal.bordeaux').modal('show');    
+    }
+
+    else if (code === "FR-V"){
+      $('.ui.modal.rhone').modal('show');   
+    }
+
+    else if (code === "FR-D"){
+      $('.ui.modal.burgundy').modal('show');   
+    }
+
+    else if (code === "AR-M"){
+       $('.ui.modal.mendoza').modal('show');       
+    }
+
+    else if (code === "AR-A"){
+      $('.ui.modal.salta').modal('show');  
+    }
+
+    else if (code === "ES-BU"){
+      $('.ui.modal.ribera').modal('show');       
+    }
+
+    else if (code === "ES-LO"){
+      $('.ui.modal.rioja').modal('show');       
+    }
+
+    else {
+      console.log('nothing');
+    }
+  }
 
 
 
